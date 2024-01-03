cb91b879080debf1ece19d391a00bf42b7856471
swainsubrat <mailofswainsubrat@gmail.com>
2024-01-03 22:46:56 +1000

    PANDA project start

--- README.md
+++ README.md
@@ -1,24 +1,41 @@
 # PANDA
-PANDA
+PANDA: **P**ractical **A**dversarial Attack Against I**n**trusion **D**etection **A**pplications
 
-## Dataset
-We're using the [UQ-IoT dataset](https://espace.library.uq.edu.au/view/UQ:17b44bb) for all the experiments.
+## Model Training
+### Dataset
+We're using the [UQ-IoT dataset](https://espace.library.uq.edu.au/view/UQ:17b44bb) for all the experiments. Why?
+1. It was created using our in-house IoT testbed at The UQ, Australia.
+2. A huge dataset containing diverse network attacks.
+3. It will help replaying the adversarial attack on the platform.
+
+### ML Model (Surrogate Model)
+Our goal is to launch practical adversarial attack against Intrusion Detection Systems (IDS). To achieve this goal, we're creating an ML model pipeline that satisfies this two criteria:
+1. The created ML pipeline (Packets -> Features -> Model) should give similar performance as the target IDS.
+2. The created ML pipeline should facilitate easy gradient propagation to the input layer (problem space).
+
+So that, this ML model can be attacked and the obtained adversarial attack traffic from the surrogate model can be repurposed to attack the target model, called transferability property of adversarial attacks.
+
+Possible Surrogate Model:
+1. CNN-based IDS
+2. Temporal CNN
+3. Other sequential models or Transformers
+
 ### Training
 For training we used a small porton of the dataset. We used a small portion because the anomaly detection model is relatively small (for edge devices), and using a lot of data might result in overfitting.
 
 ### Testing
 For testing, in the initial phase, we're using 120k benign samples and 6 attacks from different devices. Later, the entire dataset will be used for testing.
 
-## Anomaly Score
+### Anomaly Score
 Differs from model to model. Here, -ve of the reonstruction error.
 
-## Threshold
+### Threshold
 This section covers, how we decide on the threshold for the anomaly detection. As a general convention, "An example is said to be anomalous, if the anomaly score is more than a pre-defined threshold". The definition of anomaly score is different for different models, e.g., for autoencoder, it's the reconstruction error. The threshold is calculated generally during training time with training set or validation set. Below are the various methods to calculate threshold.
 
-### Use a percentile of the reconstruction error distribution.
+#### Use a percentile of the reconstruction error distribution.
 This is a simple and effective approach. We calculate the reconstruction error for each example in the training dataset and then identify the percentile of the reconstruction error distribution. For example, 95th percentile of the reconstruction error distribution. This means that any example with a reconstruction error greater than the 95th percentile would be considered anomalous.
 
-### Attack
+## Adversarial Attack
 So far, we have tried attacks such as:
 - FGSM
 - PGD
--- code/attacks.py
+++ code/attacks.py
@@ -7,6 +7,15 @@ from torchvision import transforms
 from models import Autoencoder
 from datasets import PcapDataset
 
+# Define transformations (if needed)
+transform = transforms.Compose([
+    # Add any desired transformations here
+])
+# Define loss function (Binary Cross-Entropy Loss for binary data)
+criterion = nn.BCELoss()
+mask = torch.zeros(235, 235)
+mask[:, :32] = 1
+
 def get_timegaps(packet):
     first_32_bits = packet[:, :32]
     integer_values = []
@@ -17,6 +26,68 @@ def get_timegaps(packet):
 
     return integer_values, sum(integer_values)
 
+def evaluate_perturbed_packets():
+    ...
+
+def fgsm(pcap_path, epsilon):
+    # Load the trained autoencoder model
+    model = Autoencoder()
+    model.load_state_dict(torch.load('../artifacts/models/autoencoder_model_best.pth'))
+    model.eval()
+
+    batch_size = 1
+
+    # Create the DataLoader
+    dataset = PcapDataset(pcap_file=pcap_path, max_iterations=sys.maxsize, transform=transform)
+    dataloader = DataLoader(dataset, batch_size=235 * batch_size, shuffle=False, drop_last=True)
+
+    re = []
+    adv_re = []
+    y_true, y_pred = [], []
+    taus = []
+    total_time = 0
+    adv_total_time = 0
+    for packets in dataloader:
+        _, sum_clean_timegaps = get_timegaps(packets)
+        total_time = total_time + sum_clean_timegaps
+        reshaped_packets = packets.reshape(batch_size, 1, 235, 235).to(torch.float)
+        reshaped_packets.requires_grad = True
+        outputs = model(reshaped_packets)
+
+        # Compute the loss
+        loss = criterion(outputs, reshaped_packets)
+        re.append(loss.data)
+        model.zero_grad()
+
+        loss.backward()
+
+        sign_data_grad = reshaped_packets.grad.data.sign()
+
+        # Create the perturbed image by adjusting the original image
+        perturbed_packets = reshaped_packets + (epsilon * sign_data_grad) * mask
+
+        # Clip the perturbed image to ensure it stays within valid data range
+        perturbed_packets = torch.clamp(perturbed_packets, 0, 1)
+
+        adv_outputs = model(perturbed_packets)
+        adv_loss = criterion(adv_outputs, perturbed_packets)
+        adv_anomaly_score = -1 * adv_loss.data
+        y_true.append(1 if "malicious" in pcap_path else 0)
+        y_pred.append(1 if adv_anomaly_score > -0.2661924958229065 else 0)
+
+        adv_re.append(adv_loss.data)
+
+        perturbed_packets = perturbed_packets.squeeze()
+        adv_timegaps, sum_adv_timegaps = get_timegaps(perturbed_packets)
+        adv_total_time = adv_total_time + sum_adv_timegaps
+        taus = taus + adv_timegaps
+
+    print(f"Total time: {total_time}, Adv: {adv_total_time}")
+
+    return re, adv_re, y_true, y_pred, taus
+
+def pgd(pcap_path, epsilon):
+    ...
 
 import torch
 from torch.utils.data import DataLoader
@@ -29,19 +100,16 @@ class Attack:
         self.mask = torch.zeros(235, 235)
         self.pcap_path = args.pcap_path
         self.mask[:, :32] = 1
-        self.device = torch.device(args.device)
-
-        self.transform = transforms.Compose([
-            # Add any desired transformations here
-        ])
-        self.dataset = PcapDataset(pcap_file=self.pcap_path, max_iterations=sys.maxsize, transform=self.transform)
-        self.dataloader = DataLoader(self.dataset, batch_size=235 * self.batch_size, shuffle=False, drop_last=True)
-        self.criterion = nn.BCELoss()
+        # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = args.device
         self.model = Autoencoder().to(self.device)
         self.model.load_state_dict(torch.load(self.model_path))
         self.model.eval()
 
     def fgsm(self, epsilon):
+        dataset = PcapDataset(pcap_file=self.pcap_path, max_iterations=sys.maxsize, transform=transform)
+        dataloader = DataLoader(dataset, batch_size=235 * self.batch_size, shuffle=False, drop_last=True)
+
         re = []
         adv_re = []
         y_true, y_pred = [], []
@@ -49,7 +117,7 @@ class Attack:
         total_time = 0
         adv_total_time = 0
 
-        for packets in self.dataloader:
+        for packets in dataloader:
             _, sum_clean_timegaps = get_timegaps(packets)
             total_time = total_time + sum_clean_timegaps
             reshaped_packets = packets.reshape(self.batch_size, 1, 235, 235).to(torch.float).to(self.device)
@@ -57,7 +125,7 @@ class Attack:
             outputs = self.model(reshaped_packets)
 
             # Compute the loss
-            loss = self.criterion(outputs, reshaped_packets)
+            loss = criterion(outputs, reshaped_packets)
             re.append(loss.data)
             self.model.zero_grad()
 
@@ -72,7 +140,7 @@ class Attack:
             perturbed_packets = torch.clamp(perturbed_packets, 0, 1)
 
             adv_outputs = self.model(perturbed_packets)
-            adv_loss = self.criterion(adv_outputs, perturbed_packets)
+            adv_loss = criterion(adv_outputs, perturbed_packets)
             adv_anomaly_score = -1 * adv_loss.data
             y_true.append(1 if "malicious" in self.pcap_path else 0)
             y_pred.append(1 if adv_anomaly_score > -0.2661924958229065 else 0)
